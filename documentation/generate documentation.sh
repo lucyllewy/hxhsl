838e5a9aa84305d15c1025480b50a55fca8d421c
pimmhogeling <pimmhogeling@8e778216-d376-11de-bb02-e302c5db49d5>
2010-06-26 14:00:28 +0000

    Updated documentation
    git-svn-id: http://hxhsl.googlecode.com/svn/trunk@86 8e778216-d376-11de-bb02-e302c5db49d5

--- documentation/generate documentation.sh
+++ documentation/generate documentation.sh
@@ -1,3 +1,3 @@
-haxe Importer -cp "../hsl-pico source" -cp "../hsl source" -cp "/home/pimm/Bureaublad/haxe/haxe/std" -xml "documentation.xml"
+haxe Importer -cp "../hsl-pico source" -cp "../hsl source" -xml "documentation.xml"
 cp "documentation.xml" "../hsl-pico source/haxedoc.xml"
 mv "documentation.xml" "../hsl source/haxedoc.xml"
