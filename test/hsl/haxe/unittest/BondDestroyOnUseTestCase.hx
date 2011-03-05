810d5cbe05ac8a37517a13603033024885e419f6
pimmhogeling <pimmhogeling@8e778216-d376-11de-bb02-e302c5db49d5>
2011-03-05 21:12:05 +0000

    Added missing unit test
    git-svn-id: http://hxhsl.googlecode.com/svn/trunk@129 8e778216-d376-11de-bb02-e302c5db49d5

---
+++ test/hsl/haxe/unittest/BondDestroyOnUseTestCase.hx
@@ -0,0 +1,25 @@
+﻿package hsl.haxe.unittest;
+import hsl.haxe.Bond;
+
+class BondDestroyOnUseTestCase extends UnitTestCaseBase {
+	private var cat:Cat;
+	private var dog:Dog;
+	public function new():Void {
+		super();
+	}
+	public override function setup():Void {
+		cat = new Cat();
+		dog = new Dog();
+	}
+	public function testBondDestroyingOnUse():Void {
+		assertFalse(cat.isRunning);
+		dog.barkedSignaler.bindVoid(cat.startRunning).destroyOnUse();
+		assertFalse(cat.isRunning);
+		dog.bark();
+		assertTrue(cat.isRunning);
+		cat.reset();
+		assertFalse(cat.isRunning);
+		dog.bark();
+		assertFalse(cat.isRunning);
+	}
+}
